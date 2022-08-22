79690410607a4c72155e2d3c2cad04f24c895176
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-22 17:30:23 +0530

    Added a button to navigate from the main activity to DiceRoller activity

--- app/src/main/java/com/dreamsoft/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/dreamsoft/kotlinbasics/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dreamsoft.kotlinbasics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickBtn(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
