a65fa1dbbd8eef2ce3a87109ea54a8d28380c7de
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-24 07:49:12 +0530

    Modified DiceRoller.kt file

--- app/src/main/java/com/dreamsoft/kotlinbasics/DiceRoller.kt
+++ app/src/main/java/com/dreamsoft/kotlinbasics/DiceRoller.kt
@@ -2,10 +2,21 @@ package com.dreamsoft.kotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class DiceRoller : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
+        val btn_roll : Button = findViewById(R.id.btn_roll)
+        val tv_result : TextView = findViewById(R.id.tv_result)
+
+        btn_roll.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            tv_result.text = "6";
+        }
     }
 }
