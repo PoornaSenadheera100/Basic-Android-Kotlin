a6f260c3f45a6697197040b58a95a299f30fc1f5
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-24 16:57:40 +0530

    Modified LuckyDiceRoll.kt file

--- app/src/main/java/com/dreamsoft/kotlinbasics/LuckyDiceRoll.kt
+++ app/src/main/java/com/dreamsoft/kotlinbasics/LuckyDiceRoll.kt
@@ -2,10 +2,22 @@ package com.dreamsoft.kotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LuckyDiceRoll : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_dice_roll)
+
+        val luckyNumber : EditText = findViewById(R.id.et_luckyNumber)
+        val result : TextView = findViewById(R.id.tv_resultLuckyRoll)
+        val btn_roll : Button = findViewById(R.id.btn_luckyRoll)
+
+        btn_roll.setOnClickListener {
+            val number : Int = (1..6).random()
+            result.text = number.toString()
+        }
     }
 }
